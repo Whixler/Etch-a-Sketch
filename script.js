2f87620da9e74da81ebb6e0cebaea821e8f07ff6
Lotte <obdeijn.lotte@gmail.com>
2025-05-20 15:59:36 +0200

    added button with popup prompt

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ const box = document.querySelector("#container");
 
 for(i=1; i<=256; i++){
 const content = document.createElement("div");
-content.classList.add("content");
+content.classList.add("box");
 content.textContent = i;
 
 box.appendChild(content);
 }
+
+document.querySelector("button").addEventListener('click', () => {
+    let gridSize = window.prompt("Enter gridsize", 16);
+    console.log(gridSize);
+})
 
 
