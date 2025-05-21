7b8deb8796ee1ea5a5e1e6b3e7f14f2abe58e193
Lotte <obdeijn.lotte@gmail.com>
2025-05-21 08:37:53 +0200

    added remove function

--- script.js
+++ script.js
@@ -2,19 +2,22 @@ const box = document.querySelector("#container");
 let gridSize = 16;
 
 function createBox(gridSize){
-for(i=1; i<=(gridSize*gridSize); i++){
-const content = document.createElement("div");
-content.classList.add("box");
-content.textContent = i;
-
-box.appendChild(content);
+    for(i=1; i<=(gridSize*gridSize); i++){
+        const content = document.createElement("div");
+        content.classList.add("box");
+        content.textContent = i;
+        box.appendChild(content);
 }}
+
+function removeBox(){
+    box.innerHTML = "";
+}
 
 document.querySelector("button").addEventListener('click', () => {
     let gridSize = window.prompt("Enter gridsize", 16);
     console.log(gridSize);
-    createBox(gridSize)
-
+    removeBox();
+    createBox(gridSize);
 })
 
 
