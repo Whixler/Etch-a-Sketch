6503c062f5eb2dadf8f093dec33cd4ad60d87009
Lotte <obdeijn.lotte@gmail.com>
2025-05-21 09:54:45 +0200

    added hover function to keep color changed.

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 const box = document.querySelector("#container");
-let widthBox;
 let gridSize = 16;
 
 function createGrid(size){
     box.innerHTML = ""
-    widthBox = 640 / gridSize - 2;
+    let widthBox = 640 / gridSize - 2;
 
     for(i=1; i<=(gridSize*gridSize); i++){
         const cell = document.createElement("div");
-        cell.classList.add("box");
+        cell.classList.add("cell");
         cell.style.width = widthBox + "px";
         cell.style.height = widthBox + "px";
         box.appendChild(cell);
 }}
 
-
+function hover(){
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = "orange";
+        })        
+    })
+}
 
 document.querySelector("button").addEventListener('click', () => {
     gridSize = window.prompt("Enter gridsize", 16);
@@ -22,3 +28,4 @@ document.querySelector("button").addEventListener('click', () => {
 })
 
 createGrid(16);
+hover();
