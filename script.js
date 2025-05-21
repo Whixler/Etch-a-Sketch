a54a7ba872f3aeaeefac15d6a00d3524fba4bcb3
Lotte <obdeijn.lotte@gmail.com>
2025-05-21 10:09:51 +0200

    added reset button

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let gridSize = 16;
 function createGrid(size){
     box.innerHTML = ""
     let widthBox = 640 / gridSize - 2;
+    hover()
 
     for(i=1; i<=(gridSize*gridSize); i++){
         const cell = document.createElement("div");
@@ -22,10 +23,23 @@ function hover(){
     })
 }
 
-document.querySelector("button").addEventListener('click', () => {
+document.querySelector("#gridSize").addEventListener('click', () => {
     gridSize = window.prompt("Enter gridsize", 16);
     createGrid(gridSize);
+    hover();
 })
+
+document.querySelector("#reset").addEventListener('click', () =>{
+    reset();
+    hover();
+})
+
+function reset(){
+        const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = "#1F2937";     
+    })
+}
 
 createGrid(16);
 hover();
